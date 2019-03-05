8b2d8cd658c9237eccf7f9a754d0fe9cc0364882
juancp92 <juanc.plaza92@gmail.com>
2019-03-05 01:33:55 -0300

    Added launcher icon

--- app/src/main/java/e/juanplaza/notetaker/DatabaseDataWorker.java
+++ app/src/main/java/e/juanplaza/notetaker/DatabaseDataWorker.java
@@ -16,17 +16,44 @@ public class DatabaseDataWorker {
 
     public void insertCourses() {
         insertCourse("TP1", "Taller de Programacion 1");
-        insertCourse("M1", "Matematica 1");
+        insertCourse("AM1", "Analisis Matematico 1");
         insertCourse("LS", "Logica Simbolica");
         insertCourse("TG", "Teoria de Grafos");
+        insertCourse("IT1", "Ingles Tecnico 1");
+        insertCourse("AM2", "Analisis Matematico 2");
+        insertCourse("AE1", "Algoritmos y Estructura de Datos 1");
+        insertCourse("AC1", "Arquitectura de las Computadoras 1");
+        insertCourse("TP2", "Taller de Programacion 2");
+        insertCourse("IT2", "Ingles Tecnico 2");
+        insertCourse("AE2", "Algoritmo y Estructura de Datos 2");
+        insertCourse("DOO", "Diseno Orientado a Objetos");
+        insertCourse("BD", "Bases de Datos");
+        insertCourse("PyE", "Probabilidad y Estadistica");
+        insertCourse("AC2", "Arquitectura de las Computadoras 2");
+        insertCourse("SO", "Sistemas Operativos");
+        insertCourse("RC1", "Redes de Computadoras 1");
+        insertCourse("TP3", "Taller de Programacion 3");
+        insertCourse("TL", "Teoria de Lenguajes");
+        insertCourse("SI", "Seguridad Informatica");
+        insertCourse("RC2", "Redes de Computadoras 2");
+        insertCourse("IS1", "Ingenieria del Software 1");
+        insertCourse("TP4", "Taller de Programacion 4");
+        insertCourse("IS2", "Ingenieria del Software 2");
+        insertCourse("PP", "Programacion Profesional");
+        insertCourse("GO", "Gestion de las Organizaciones");
+        insertCourse("S1N", "Seminario 1 .NET");
+        insertCourse("S2A", "Seminario 2 Android");
+        insertCourse("S3L", "Seminario 3 Linux Embebido");
+
     }
 
     public void insertSampleNotes() {
-        insertNote("TP1", "Punteros", "Los punteros son la forma de referenciar en memoria");
-        insertNote("TP1", "C", "C es el lenguaje de programacion utilizado en la materia");
+        insertNote("TP1", "C", "C es el lenguaje de programacion que sera utilizado en la materia");
+        insertNote("AC2", "Pipeline", "Optimizacion para la ejecucion de las instrucciones en un procesador");
+        insertNote("RC2", "UDP", "Protocolo IP basado en datagramas no orientado a conexion y no fiable");
+        insertNote("IS2", "Proyecto Final", "El proyecto final consiste en realizar el relevamiento, diseno e implementacion de un software de facturacion");
+        insertNote("BD", "Normalizacion", "Proceso por el cual se realiza un modelo de datos para cumplir con las formas normales");
 
-        insertNote("TG", "Grafo Dirigido", "Grafo en el cual los aristas deben ser recorridos en un solo sentido");
-        insertNote("TG", "Dijkstra", "Algoritmo de menor recorrido de grafos con peso en las arista");
     }
 
     private void insertCourse(String courseId, String title) {
